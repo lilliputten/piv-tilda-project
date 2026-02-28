b3f147e7bf694c684e8eaadab3cfe2ff1d984b90
lilliputten <lilliputten@gmail.com>
2026-02-28 14:36:47 +0300

    Updated hero gallery style.

--- src/HeroGallery.ts
+++ src/HeroGallery.ts
@@ -1,3 +1,46 @@
+let owlCarousel: JQuery<HTMLDivElement> | undefined;
+
+function initCarousel() {
+  const slides = [
+    // Gallery slide ids. See also styles in the `src/styles/HeroGallery.scss` module.
+    '#rec1966648601',
+    '#rec1972774181',
+  ];
+  const jqSlides = $(slides.join(', '));
+  const block = $('.uc-HeroGallery');
+  const wrap = $(block.find('.carousel-wrapper'));
+  owlCarousel = wrap.find('.tn-atom');
+  owlCarousel.addClass('owl-carousel owl-theme');
+  jqSlides.appendTo(owlCarousel);
+
+  // Initialize owl carousel with responsive breakpoints
+  owlCarousel.owlCarousel({
+    loop: true,
+    center: true,
+    dots: true,
+    nav: true,
+    margin: 0,
+    items: 1,
+    navText: ['❮', '❯'],
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+  });
+
+  jqSlides.addClass('inited');
+}
+
+let updateGemetryHandler: NodeJS.Timeout;
+
+function updateGemetry() {
+  clearTimeout(updateGemetryHandler);
+  updateGemetryHandler = setTimeout(() => {
+    requestAnimationFrame(() => {
+      owlCarousel?.trigger('refresh.owl.carousel');
+    });
+  }, 1000);
+}
+
 export function initHeroGallery() {
   const heroGallerySelector = '.uc-HeroGallery';
   const heroGallery = document.querySelector<HTMLDivElement>(heroGallerySelector);
@@ -7,8 +50,7 @@ export function initHeroGallery() {
     return;
   }
 
-  console.log('[HeroGallery] Initialized', {
-    heroGallery,
-  });
-  debugger;
+  initCarousel();
+
+  window.addEventListener('resize', updateGemetry);
 }
